1edd281047e4df99ab1514a9899aff36bf9d06ac
Kai Kao <handsomekai0530@gmail.com>
2012-11-17 21:24:53 +0800

    add car insurance folder

---
+++ public/car_insurance/Index.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Index</title>
+		<link href="css/Index.css" type="text/css" rel="stylesheet">
+	</head>
+	
+	<body>
+		<div class="title">
+		   <h1>
+		   	   California Auto Insurance Guide
+		   </h1>
+		</div>
+		
+		<ul class="navigation">
+			<li>
+				<a href="#">Home</a>
+			</li>
+			<li>
+				<a href="#">Requirements & Laws</a>
+			</li>
+			<li>
+				<a href="#">Discounts</a>
+			</li>
+			<li>
+				<a href="#">FAQs</a>
+			</li>
+			<li>
+				<a href="#">Directory</a>
+			</li>
+			<li>
+				<a href="#">About us</a>
+			</li>
+			
+		</ul>
+		
+		<div id="preview">
+			<div class="preview_left">
+		 	    <div class="getquote">
+				     <h2>Save up to $520 this year</h2>
+                     <p>
+                         Get the Best CA Auto Insurance Quote Now!
+                     </p>
+                     <input type="text" />
+                       <a href="#" >
+                       	  <span>
+                       	  	Get Quote
+                       	  </span>
+                       </a>
+            	</div>
+             	<div class="enterzip">
+			         <h2>Need to file a claim?</h2>
+                     <p>
+                        Check out our directory of popular insurance companies' 
+                        accident and claim contact information.
+                     </p>
+			    </div>
+		   </div>
+			<div class="preview_right">
+				<span class="cartext">
+					Looking For The 
+				</span>
+				<span class="cartext2">
+					Best Insurance?
+				</span>
+				<span class="cartext3">
+					&#163 Stop paying too much <br /> &#163 learn how to save
+				</span>
+			    <a href="#" class="carbutton">
+			    	<span>
+			    		Click to learn more
+			    	</span>
+			    </a>
+			</div>
+		</div>
+		<div id="discount_background">
+		<div id="discount">
+			<div class="top5">
+				<div class="top5_picture">
+		            <a href="#">
+		            	<img src="img/Discount.jpg" alt="discount picture">
+		            </a>
+			    </div>
+			    <div class="top5_paragraph">
+				   	<h3>Top 5 Insurance Discount</h3>
+			  	    <p>1. Good Student Discount<br />
+			  	       2. Safe Driver Discount<br />
+			  	       3. Multiple Car Discount<br />
+			  	       4. Club Member....
+			  	    </p>
+			  	</div>
+			  	<div class="top5_cont">
+					<a href="#">
+						<span>
+							(continue)
+						</span>
+					</a>
+				</div>
+			</div>
+			<div class="agent">
+				<div class="agent_picture">
+					<a href="#">
+						<img src="img/agency.jpg" alt="agency picture">
+					</a>
+				</div>
+				<div class="agent_paragraph">
+					<h3>Need Insurance Agency in your area?</h3>
+					<p>Browse our comprehensive directory of California auto insur....    </p>
+				</div>
+				<div class="agent_cont">
+					<a href="#">
+						<span>
+							(continue)
+						</span>
+					</a>
+				</div>
+			</div>
+			<div class="facebook">
+				<div class="facebook_picture">
+					<a href="#">
+						<img src="img/facebook.jpg" alt="facebook picture">
+					</a>
+				</div>
+				<div class="facebook_paragraph">
+					<h3>Take our Facebook Quiz!</h3>
+					<p>Take our awesome Facebook quiz and find out if you are a careless...</p>
+				</div>
+				<div class="facebook_cont">
+					<a href="#">
+						<span>
+							(continue)
+						</span>
+					</a>
+				</div>
+			</div>
+		 </div>
+		</div>
+		<div id="feedbackground">
+		<div id="feedback">
+			<div class="news">
+				<h2>
+					News
+				</h2>
+				<h3>
+					Check what's new around the country
+				</h3>
+			    <ul>
+			    	<li>
+			    		<a href="#">
+			    			I am news here I am news here I am news hereI am news here I am news here I am news here
+			    		</a>
+			    	</li>
+			    	<li>
+			    		<a href="#">
+			    			I am news here I am news here I am news hereI am news here I am news here I am news here
+			    		</a>
+			    	</li>
+			    	<li>
+			    		<a href="#">
+			    			I am news here I am news here I am news hereI am news here I am news here I am news here
+			    		</a>
+			    	</li>
+			    	<li>
+			    		<a href="#">
+			    			I am news here I am news here I am news hereI am news here I am news here I am news here
+			    		</a>
+			    	</li>
+			    	
+			    </ul>
+			</div>
+			<div class="story">
+				<h2>
+					I am title here  I am title here I am title here
+				</h2>
+				<div class="story_left_top">
+					<a href="#">
+						<img  src="img/happy-people.jpg">
+					</a>
+					<h3 >
+						Getting the Right Auto Insurance
+					</h3>
+					<p>
+						With a population of 37 million, California has the largest number 
+						of drivers in the US. More people means more drivers and unfortunately, 
+						more accidents. This is why having the right...
+					</p>
+					<div class="story_left_top_cont">
+					<a href="#">
+						<span>
+							(continue)
+						</span>
+					</a>
+				</div>
+				</div>
+				<div class="story_right_top">
+					<h3>
+						Common Questions
+					</h3>
+					<img  src="img/question.jpg">
+					<ul>
+						<li>
+							<a href="#">
+								What is a deductible?
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								What is Gap Coverage?
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								Does credit score affect rates?
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								What if I am denied coverage?
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								What is Collision Coverage?
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								What is PIP insurance?
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								See all Q&As
+							</a>
+						</li>
+					</ul>
+					
+				</div>
+				<div class="story_left_bottom">
+					<h3>
+						Quick CA Insurance Facts
+					</h3>
+					<ul>
+						<li>
+							<a href="#">
+								Liability insurance is required in California
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								Texting and Driving is against the Law
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								Blah balh
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								Blah balh
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								WLiability insurance is required in California
+							</a>
+						</li>
+					</ul>	
+				</div>
+				<div class="story_right_bottom">
+					Available blank column
+				</div>
+			</div>
+		</div>
+		</div>
+		<div id="footer">
+			&copy; 2012 Best California Auto Insurance. All rights reserved.
+		</div>
+		
+		
+	</body>
+	
+	
+	
+</html>
