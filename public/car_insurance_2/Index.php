11c1c5754ce8bcd256f05eb1fff2143315fa9594
Kai Kao <handsomekai0530@gmail.com>
2012-11-25 23:57:20 +0800

    car insurance template version 2

---
+++ public/car_insurance_2/Index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Index</title>
+		<link href="css/Index.css" rel="stylesheet" type="text/css">
+	</head>
+
+
+	<body>
+		<div id="full_content">
+			<div id="title">
+				<h1>California <span>Car Insurance</span> Guide</h1>
+			</div>
+			
+			<div id="navigation">
+				<ul>
+					<li>
+						<a href="#">
+							Home
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							Requirement & Laws
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							Discounts
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							FAQs
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							Directory
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							About us
+						</a>
+					</li>
+				</ul>
+			</div>
+			
+			<div id="preview">
+				<h1>Find The Best California Auto Insurance!</h1>
+				<p>Save up to $520 This year</p>
+				<input class="quote_email" type="text" title="Email Address" placeholder="Enter Zip Code" />
+				<a href="#" class="carbutton">
+			    	<span>
+			    		Get Quote &rarr;
+			    	</span>
+			    </a>
+				<img src="Img/benz small.jpg" alt="discount picture">
+			</div>
+			
+			<div id="discount">
+				<div class="discount_left">
+					<div class="right_insurance">
+						<h1>Getting The Right Auto Insurance</h1>
+						<img src="Img/happy-people.jpg" alt="happy people">
+						<p>
+							With a population of 37 million, California has the 
+							largest number of drivers in the US. More people means 
+							more drivers and unfortunately, more accidents. This 
+							is why having the right auto insurance coverage is important 
+							for California drivers. With hundreds of auto insurance companies 
+							to choose from, we understand how difficult it can be sifting 
+							through websites... 
+						</p>
+					</div>
+					<div class="questions">
+						<h1>
+						Common Questions
+						</h1>
+						
+						<ul>
+							<li>
+								<a href="#">
+									What is a deductible?
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									What is Gap Coverage?
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Does credit score affect rates?
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									What if I am denied coverage?
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									What is Collision Coverage?
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									What is PIP insurance?
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									See all Q&As
+								</a>
+							</li>
+						</ul>
+						
+					</div>
+					<div class="ca_facts">
+						<h1>
+							Quick CA Insurance Facts
+						</h1>
+						<ul>
+							<li>
+								<a href="#">
+									Liability insurance is required in California
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Texting and Driving is against the Law
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Blah balh
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Blah balh
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									See all CA Insurance Laws
+								</a>
+							</li>
+					</ul>	
+					</div>
+				</div>
+				<div class="discount_right">
+					<div class="top5">
+						<a href="#">
+							<img src="Img/Discount.jpg" alt="discount">
+						</a>	
+						<h2>
+							Top 5 Insurance Discount
+						</h2>
+						 <p>
+						 	1. Good Student Discount<br />
+					  	    2. Safe Driver Discount<br />
+					  	    3. Multiple Car Discount<br />
+					  	    4. Club Member....
+			  	    	</p>
+					</div>
+					<div class="agent">
+						<a href="#">
+							<img src="Img/agency.jpg" alt="agency">
+						</a>
+						<h2>
+							Need agent around ?
+						</h2>
+						<p>
+							Browse our comprehensive directory of 
+							California auto insurance agents.
+						</p>
+					</div>
+					<div class="facebook">
+						<a href="#">
+							<img src="Img/facebook.jpg" alt="agency">
+						</a>
+						<h2>
+							Take our facebook quiz!
+						</h2>
+						<p>
+							Take our awesome Facebook quiz 
+							and find out if you are a careless 
+							or cautious driver!
+						</p>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		
+	
+	
+	</body>
+
+</html>
